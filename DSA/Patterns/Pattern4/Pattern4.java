2f8683a15620a98aaff2aa830ef0a3c92fafbc33
Rahul <rahul@Rahuls-MacBook-Air.local>
2026-05-12 09:04:51 +0530

    Day-2

---
+++ DSA/Patterns/Pattern4/Pattern4.java
@@ -0,0 +1,12 @@
+package DSA.Patterns.Pattern4;
+
+public class Pattern4 {
+    public static void main(String[] args){
+        for(int i=4;i>0;i--){
+            for(int j=0;j<i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
