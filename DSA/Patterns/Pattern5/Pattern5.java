5ee387c6e9ca15866b373dd07290c5d999191f36
Rahul <rahul@Rahuls-MacBook-Air.local>
2026-05-12 09:17:26 +0530

    Day-2

---
+++ DSA/Patterns/Pattern5/Pattern5.java
@@ -0,0 +1,12 @@
+package DSA.Patterns.Pattern5;
+
+public class Pattern5 {
+    public static void main(String[] args){
+        for(int i=4;i>0;i--){
+            for(int j=0;j<i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ DSA/Patterns/Pattern6/Pattern6.java
@@ -0,0 +1,12 @@
+package DSA.Patterns.Pattern6;
+
+public class Pattern6 {
+    public static void main(String[] args){
+        for(int i=0;i<5;i++){
+            for(int j=0;j<i;j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
